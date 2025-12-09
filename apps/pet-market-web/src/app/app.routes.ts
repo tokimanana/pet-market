ef642b5f1974bec5f3975233b9a27685d9dae813
tokimanana <tokimananasarobidy@gmail.com>
2025-12-09 23:05:51 +0300

    feat(frontend -> products):  header, home and update routing

--- apps/pet-market-web/src/app/app.routes.ts
+++ apps/pet-market-web/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const mod = await import('./home/home');
+      return mod.Home;
+    }
+  },
+  {
+    path: 'products',
+    loadComponent: async () => {
+      const mod = await import('./products/products');
+      return mod.Products;
+    },
+  }
+];
