39ce65a1150c417743c9b509741d849cb935dd5f
tokimanana <tokimananasarobidy@gmail.com>
2025-12-19 14:21:42 +0300

    feat(frontend): Add cart icon in the header with items count

--- apps/pet-market-web/src/app/components/header/header.ts
+++ apps/pet-market-web/src/app/components/header/header.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartStore } from '../../stores/cart';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  readonly cartStore = inject(CartStore);
+  previousCount = 0;
+  isCartBouncing = signal(false);
+
+  constructor() {
+    effect(() => {
+      const currentCount = this.cartStore.totalItems();
+      if (currentCount && currentCount > this.previousCount) {
+        this.isCartBouncing.set(true);
+        setTimeout(() => {
+          this.isCartBouncing.set(false);
+        }, 1000);
+      }
+      this.previousCount = currentCount;
+    })
+  }
+}
--- apps/pet-market-web/src/app/stores/cart.ts
+++ apps/pet-market-web/src/app/stores/cart.ts
@@ -1,5 +1,12 @@
-import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
-import { Product } from "@prisma/client";
+import { computed } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { Product } from '@prisma/client';
 
 type CartItem = Product & { quantity: number };
 
@@ -13,33 +20,45 @@ const initialState: CartState = {
 
 export const CartStore = signalStore(
   {
-    providedIn: 'root'
+    providedIn: 'root',
   },
   withState(() => initialState),
+  withComputed((store) => ({
+    totalItems: computed(() =>
+      store.items().reduce((acc, item) => {
+        return acc + item.quantity;
+      }, 0)
+    ),
+  })),
   withMethods((store) => ({
     addToCart(product: Product, quantity = 1) {
       const currentItems = store.items();
-      const itemInCart = currentItems.find(cartItem => cartItem.id === product.id);
+      const itemInCart = currentItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
 
-      if(itemInCart) {
+      if (itemInCart) {
         const updatedItems = store.items().map((cartItem) => {
-          if(cartItem.id === product.id) {
+          if (cartItem.id === product.id) {
             return {
               ...cartItem,
-              quantity: cartItem.quantity + quantity
-            }
+              quantity: cartItem.quantity + quantity,
+            };
           }
           return cartItem;
-        })
+        });
         patchState(store, { items: updatedItems });
       } else {
         patchState(store, {
-          items: [...store.items(), {
-            ...product,
-            quantity
-          }]
-        })
+          items: [
+            ...store.items(),
+            {
+              ...product,
+              quantity,
+            },
+          ],
+        });
       }
-    }
+    },
   }))
-)
+);
