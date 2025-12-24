e692ef23f10d9937c168dbd29ea31e368b80bc5d
tokimanana <tokimananasarobidy@gmail.com>
2025-12-24 13:12:32 +0300

    feat(backend): Created resources: checkout and orders. Installed stripe dependency

--- apps/pet-market-ko/src/app/app.module.ts
+++ apps/pet-market-ko/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ProductsModule } from './products/products.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { ProductsModule } from './products/products.module';
       autoSchemaFile: join(process.cwd(), 'apps/pet-market-ko/dist/schema.gql'),
     }),
     ProductsModule,
+    CheckoutModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
