8006d9954614a2bc2326f0ffc760c386b5587bb6
tokimanana <tokimananasarobidy@gmail.com>
2025-12-30 10:49:09 +0300

    feat(frontend): created order-detail component

---
+++ apps/pet-market-web/src/app/orders/components/order-detail/order-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detail',
+  imports: [],
+  templateUrl: './order-detail.html',
+  styleUrl: './order-detail.scss',
+})
+export class OrderDetail {}
