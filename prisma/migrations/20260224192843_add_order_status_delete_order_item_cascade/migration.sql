aaf0bd3f91bc1a0f1a8f0f291ba7655ed27b79be
tokimanana <tokimananasarobidy@gmail.com>
2026-01-09 09:28:15 +0300

    feat(backend): Creating a migration with a down migration in prisma

---
+++ prisma/migrations/20260224192843_add_order_status_delete_order_item_cascade/migration.sql
@@ -0,0 +1,8 @@
+-- AlterEnum
+ALTER TYPE "OrderStatus" ADD VALUE 'PAYMENT_REQUIRED';
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
